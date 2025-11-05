918c043558254a67284d94844b767363230a31a4
Simon K <simshard@gmail.com>
2025-11-05 21:44:06 +0000

    tailwind styles and custom font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,18 @@
 module.exports = {
-  theme: {
-    extend: {
-      colors: {
-        pixel: 'var(--color-pixel)',
-        'pixel-light': 'var(--color-pixel-light)',
-        'pixel-dark': 'var(--color-pixel-dark)',
-      },
-        fontFamily: {
-        // make a named utility class: font-pixel
-        pixel: ['Departure Mono', 'monospace'],
-        // set default to the CSS var (fallback included)
-        sans: ['var(--font-pixel)', 'ui-sans-serif', 'system-ui'],
-      },
-    },
-  },
+  // theme: {
+  //   extend: {
+  //       fontFamily: {
+  //       // make a named utility class: font-pixel
+  //       pixel: ['Departure Mono', 'monospace'],
+  //       // set default to the CSS var (fallback included)
+  //       //sans: ['var(--font-pixel)', 'ui-sans-serif', 'system-ui'],
+  //     }, 
+
+  //      colors: {
+  //       pixel: 'var(--color-pixel)',
+  //       'pixel-light': 'var(--color-pixel-light)',
+  //       'pixel-dark': 'var(--color-pixel-dark)',
+  //     },
+  //   },
+  // },
 };
